171de20014485f805258da05b0663fcfe838d26c
shubhamsharma2790 <shubhamsharma2790@gmail.com>
2021-05-21 19:57:58 +0530

    Codebase cleanup
    Done codebase cleanup after creating react app using npx create-react-app tech-help-stack
    Edited template files : index.js, App.js
    Deleted template files : App.css, index.css, logo.svg, App.test.js, reportWebVitals.js, setupTests.js

---
+++ tech-help-stack/src/App.js
@@ -0,0 +1,8 @@
+
+function TechHelpStackApp() {
+  return (
+    <h1>TechHelpStackApp Home Test</h1>
+  );
+}
+
+export default TechHelpStackApp;
---
+++ tech-help-stack/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TechHelpStackApp from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TechHelpStackApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
